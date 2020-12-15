1868120b6a8338d64afc5c0b7edcfab8bdfecf68
Oleg Zvonilov <oleg.zvonilov@yandex.ru>
2020-12-15 18:53:21 +0300

    Refactoring

--- app.js
+++ app.js
@@ -3,28 +3,21 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import usersRouter from './routes/users';
 import cardsRouter from './routes/cards';
-import {
-  ERROR_NOT_FOUND,
-  ERROR_BAD_REQUEST,
-  ERROR_SERVER_ERROR,
-} from './units/errorsCode';
+import {ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_SERVER_ERROR,} from './units/errorsCode';
 
-const { PORT = 3000 } = process.env;
+const {PORT = 3000} = process.env;
 const app = express();
 
 const handleErrors = (err, req, res, next) => {
   if (err.name === 'CastError') {
-    res.status(ERROR_BAD_REQUEST).send({ message: `Переданы некорректные данные, параметр '${err.path}'` });
+    res.status(ERROR_BAD_REQUEST).send({message: `Переданы некорректные данные, параметр '${err.path}'`});
   } else if (err.name === 'ValidationError') {
-    const errors = Object.keys(err.errors).map((key) => {
-      const item = err.errors[key];
-      return item.message;
-    });
-    res.status(ERROR_BAD_REQUEST).send({ message: `${errors.join(', ')}` });
+    const errors = Object.values(err.errors).map((el) => el.message);
+    res.status(ERROR_BAD_REQUEST).send({message: `${errors.join(', ')}`});
   } else if (err.status === ERROR_NOT_FOUND) {
-    res.status(ERROR_NOT_FOUND).send({ message: err.message });
-  } else if (err.status === ERROR_SERVER_ERROR) {
-    res.status(ERROR_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+    res.status(ERROR_NOT_FOUND).send({message: err.message});
+  } else if (err || err.status === ERROR_SERVER_ERROR) {
+    res.status(ERROR_SERVER_ERROR).send({message: 'Произошла ошибка'});
   } else {
     next();
   }
@@ -40,10 +33,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use((err, req, res, next) => {
   if (err) {
-    res.status(ERROR_BAD_REQUEST).send({ message: 'Не верный формат данных json' });
+    res.status(ERROR_BAD_REQUEST).send({message: 'Не верный формат данных json'});
   } else {
     next();
   }
@@ -66,7 +59,7 @@ app.use(handleErrors);
 app.use((req, res) => {
   res
     .status(ERROR_NOT_FOUND)
-    .send({ message: 'Запрашиваемый ресурс не найден' });
+    .send({message: 'Запрашиваемый ресурс не найден'});
 });
 
 app.listen(PORT, () => {
